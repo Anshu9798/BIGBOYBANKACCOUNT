5fcf4a5762f4073699f15766b4b22e8dc1697c2d
Anshu9798 <42977132+Anshu9798@users.noreply.github.com>
2019-01-13 15:52:30 -0500

    Update SavingsAccount.java

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -20,6 +20,10 @@ public class SavingsAccount extends BankAccount
 		MIN_BAL = mb;
 		MIN_BAL_FEE = mbf;
 	}
+	/**
+	 * a withdraw method that specifies that balance cannot go negative after transaction
+	 * there is also a minimum balance that, if crossed, results in a fee withdrawn from the balance
+	 */
 	public void withdraw(double amt)
 	{
 		if(this.getBalance() >= amt && amt > 0)
@@ -36,6 +40,10 @@ public class SavingsAccount extends BankAccount
 		}
 			
 	}
+	/**
+	 * a standard transfer method 
+	 * once again names must be the same for both BankAccounts and balance cannot go negative
+	 */
 	public void transfer(BankAccount other, double amt)
 	{
 		if(other.getName().equals(this.getName()))
@@ -54,10 +62,16 @@ public class SavingsAccount extends BankAccount
 			throw new IllegalArgumentException("Names don't match");
 		}
 	}
+	/**
+	 * adds interest to the balance of the BankAccount
+	 */
 	public void addInterest()
 	{
 		this.deposit(((this.getBalance())*intRate));
 	}
+	/**
+	 * adds the interest to the balance of the BankAccoutn at the end of the month
+	 */
 	public void endOfMonthUpdate()
 	{
 		addInterest();
